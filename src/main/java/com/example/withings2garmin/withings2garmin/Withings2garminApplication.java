4c7f1659a18d6e779cf274bd259905928dddf468
Luca Spalierno <spalierno@gmail.com>
2024-06-29 18:30:56 +0200

    Initial commit.

---
+++ src/main/java/com/example/withings2garmin/withings2garmin/Withings2garminApplication.java
@@ -0,0 +1,13 @@
+package com.example.withings2garmin.withings2garmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Withings2garminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Withings2garminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/withings2garmin/withings2garmin/Withings2garminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withings2garmin.withings2garmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Withings2garminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
